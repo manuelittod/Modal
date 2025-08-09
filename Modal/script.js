5e2ab4aa0f41501eaa81c2eaa885eed083bf2f46
manuelittod <159807134+manuelittod@users.noreply.github.com>
2025-08-09 21:28:38 +0200

    Update script.js

--- Modal/script.js
+++ Modal/script.js
@@ -5,10 +5,11 @@ const close = document.getElementById("close");
 open.addEventListener("click", () => {
     modalContainer.classList.add("active");
 });
-
+090909090
 close.addEventListener("click", () => {
     modalContainer.classList.remove("active");
 close.addEventListener("click", () => {
     modalContainer.classList.remove("active");
 
 });
+
